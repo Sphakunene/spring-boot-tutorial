1fedba1cdf93cd1773c6c4d268ec803ff9c0315c
Sphakunene <sphacol@gmail.com>
2023-02-17 15:39:14 +0200

    added exception handling entity

---
+++ src/main/java/com/coleen/springbt/errorhandling/RestResponseExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.coleen.springbt.errorhandling;
+
+import com.coleen.springbt.entity.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
+@ResponseStatus
+public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(DepartmentNotFoundException.class)
+    public ResponseEntity<ErrorMessage> departmentNotFound(DepartmentNotFoundException departmentNotFoundException){
+        ErrorMessage errorMessage= new ErrorMessage(HttpStatus.NOT_FOUND,
+                departmentNotFoundException.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+}
