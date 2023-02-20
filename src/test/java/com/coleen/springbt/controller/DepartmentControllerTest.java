74ef1cc96d70bfb615b562e288f5ab318192a000
Sphakunene <sphacol@gmail.com>
2023-02-20 14:22:27 +0200

    added some code

--- src/test/java/com/coleen/springbt/controller/DepartmentControllerTest.java
+++ src/test/java/com/coleen/springbt/controller/DepartmentControllerTest.java
@@ -4,12 +4,19 @@ import com.coleen.springbt.entity.Department;
 import com.coleen.springbt.service.DepartmentService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @WebMvcTest(DepartmentController.class)
 class DepartmentControllerTest {
 
@@ -27,14 +34,27 @@ class DepartmentControllerTest {
                 .departmentName("IT")
                 .departmentCode("IT-06")
                 .departmentAddress("Sandton")
+                 .departmentId(1L)
                 .build();
     }
 
     @Test
-    void saveDepartment() {
+    void saveDepartment() throws Exception {
+       Department inputDepartment = Department.builder()
+                .departmentName("IT")
+                .departmentCode("IT-06")
+                .departmentAddress("Sandton")
+                 .build();
+
+        Mockito.when(departmentService.saveDepartment(inputDepartment)).thenReturn(department);
+        mockMvc.perform(post("/api/departments").contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"departmentName\": \"IT\"," +
+                        " \"departmentAddress\": \"Sandton\"," +
+                        " \"departmentCode\": \"IT-06\" },")).
+                andExpect(MockMvcResultMatchers.status().isOk());
+
+
     }
 
-    @Test
-    void getDepartmentById() {
-    }
+
 }
--- src/test/java/com/coleen/springbt/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/coleen/springbt/repository/DepartmentRepositoryTest.java
@@ -6,6 +6,7 @@
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.boot.test.mock.mockito.MockBean;
 //
 //import static org.junit.jupiter.api.Assertions.*;
 //@DataJpaTest
