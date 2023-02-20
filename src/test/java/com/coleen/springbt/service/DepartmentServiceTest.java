123cab1ccf6e8101ecdef1b02df1e15535d02fac
Sphakunene <sphacol@gmail.com>
2023-02-20 09:12:30 +0200

    added Department repository test class

--- src/test/java/com/coleen/springbt/service/DepartmentServiceTest.java
+++ src/test/java/com/coleen/springbt/service/DepartmentServiceTest.java
@@ -1,13 +1,22 @@
 package com.coleen.springbt.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class DepartmentServiceTest {
 
+    @Autowired
+    private DepartmentService departmentService;
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+
     }
 }
